fcef7ff9944e68b0cb3fc85e55e4f552e1bdc848
Maisa Salahaldin <125818947+MaisaSalahaldin@users.noreply.github.com>
2023-12-03 22:27:39 -0600

    Add files via upload

---
+++ zip/src/main/java/Green/and/Grill/GreenAndGrillApplication.java
@@ -0,0 +1,13 @@
+package Green.and.Grill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenAndGrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenAndGrillApplication.class, args);
+	}
+
+}
---
+++ zip/src/test/java/Green/and/Grill/GreenAndGrillApplicationTests.java
@@ -0,0 +1,13 @@
+package Green.and.Grill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAndGrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
